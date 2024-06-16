d2c97045bd15d16e936ac787eb6b1a9df878306a
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2024-06-16 09:38:36 +0400

    Created

---
+++ inventory-service/src/test/java/az/efiqa/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package az.efiqa.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
